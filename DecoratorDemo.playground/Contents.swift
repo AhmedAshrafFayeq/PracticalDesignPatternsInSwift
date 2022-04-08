804475de7c921518901c9d01bc00769a50e78a15
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-04-08 21:12:29 +0200

    add DecoratorClass of type UserDefaults

--- DecoratorDemo.playground/Contents.swift
+++ DecoratorDemo.playground/Contents.swift
@@ -1,3 +1,32 @@
-import UIKit
+// UserDefaults doesn't have to store and retrieve date instances
+// So I'll add set date for key, and date for methods
 
-var greeting = "Hello, playground"
+import Foundation
+
+class UserDefaultsDecorator: UserDefaults {
+    
+    private var userDefaults = UserDefaults.standard
+    
+    convenience init(userDefaults: UserDefaults) {
+        self.init()
+        self.userDefaults = userDefaults
+    }
+    
+    func set(date: Date, forKey key: String) {
+        userDefaults.set(date, forKey: key)
+    }
+    
+    func date(forKey Key: String) -> Date? {
+        return userDefaults.value(forKey: Key) as? Date
+    }
+}
+
+let userDefaults = UserDefaultsDecorator()
+userDefaults.set(42, forKey: "the answer")
+print(userDefaults.value(forKey: "the answer") ?? "-")
+
+userDefaults.set(date: Date(), forKey: "now")
+print(userDefaults.date(forKey: "now") ?? "-")
+
+// Extension can add functions and computed prop
+//but we can't override existing methods, can't add stored property and property observers
