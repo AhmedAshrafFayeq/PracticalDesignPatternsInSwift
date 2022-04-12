5892bc7207aba291ab811236f5ccd4becffee67d
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-04-12 17:11:26 +0200

    add proxy DP using lazy properties

--- ProxyDemo.playground/Contents.swift
+++ ProxyDemo.playground/Contents.swift
@@ -1,3 +1,18 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+class RandomIntWithID {
+    var value: Int = {
+        print("Value Intialized")
+        return Int.random(in: Int.min...Int.max)
+    }()
+    
+    lazy var uid: String = {
+        print("UID intialized")
+        return UUID().uuidString
+    }()
+}
+
+let n = RandomIntWithID()
+print(n.uid)
+// uid intialized after using it for the first time
+//lazy property not a thread safe
