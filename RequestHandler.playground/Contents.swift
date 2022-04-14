d0ad7a8bc495dda40cbebd8a7270685993320f60
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-04-14 16:59:10 +0200

    add chain of responsibility DP demo

--- RequestHandler.playground/Contents.swift
+++ RequestHandler.playground/Contents.swift
@@ -1,3 +1,42 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+protocol RequestHandling {
+    
+    init(next: RequestHandling?)
+    func handle(request: Any)
+}
+
+final class Handler <T>: RequestHandling, CustomStringConvertible {
+    private var nextHandler: RequestHandling?
+    
+    init(next: RequestHandling?) {
+        self.nextHandler = next
+    }
+    
+    func handle(request: Any) {
+        if request is T {
+            print("Request Processed by \(self)")
+        }else {
+            guard let handler = nextHandler else {
+                print("Reached to the end of the responder chain")
+                return
+            }
+            handler.handle(request: request)
+            print("\(self) can't handle \(T.self) requests - forwarding to \(handler)")
+        }
+    }
+    
+    var description: String {
+        return "\(T.self) Handler"
+    }
+}
+
+let dataHandler = Handler<Data>(next: nil)
+let stringHandler = Handler<String>(next: dataHandler)
+let dateHandler = Handler<Date>(next: stringHandler)
+
+let data = Data(repeating: 0, count: 10)
+dataHandler.handle(request: data)
+
+//should to end of the 
+dataHandler.handle(request: 42)
