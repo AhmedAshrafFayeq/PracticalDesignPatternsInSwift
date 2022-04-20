eaa1ffb8d32a67e1ce20718f7f1af8593f60d70e
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-04-20 17:16:22 +0200

    add Coffee machine demo with a code smell with nested loop, need to be refactored

--- CoffeeMachine.playground/Contents.swift
+++ CoffeeMachine.playground/Contents.swift
@@ -1,3 +1,44 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+class CoffeeMachine {
+    private var isWaterTankFilled: Bool
+    private var isCapsuleBinEmpty: Bool
+    private var isCapsuleInserted: Bool
+    
+    required init(waterFilled: Bool, binEmpty: Bool, capsuleInserted: Bool) {
+        isWaterTankFilled = waterFilled
+        isCapsuleBinEmpty = binEmpty
+        isCapsuleInserted = capsuleInserted
+    }
+    
+    private func isReadyToBrew() -> Bool {
+        var result = false
+        
+        if isWaterTankFilled {
+            if isCapsuleBinEmpty {
+                if isCapsuleInserted {
+                    result = true
+                    print("Coffee brewed")
+                }else {
+                    print("Insert capsule!")
+                }
+            }else {
+                print("Capsule bin full!")
+            }
+        }else {
+            print("Fill water tank!")
+        }
+        return result
+    }
+    
+    func brew() {
+        guard isReadyToBrew() else {
+            print("Can't make coffee")
+            return
+        }
+        print("coffee ready!")
+    }
+}
+
+let coffeeMachine = CoffeeMachine(waterFilled: true, binEmpty: true, capsuleInserted: false)
+coffeeMachine.brew()
